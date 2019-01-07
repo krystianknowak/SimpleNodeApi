053efa9aadc777e4d2a303fc031d2efc9aea4e00
Krystian <krystian.k.nowak@gmail.com>
2019-01-07 22:32:13 +0100

    working post + get products

--- node-api/node-rest-shop/app.js
+++ node-api/node-rest-shop/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productsRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://krystian:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-shard-00-00-stqyj.mongodb.net:27017,node-rest-shop-shard-00-01-stqyj.mongodb.net:27017,node-rest-shop-shard-00-02-stqyj.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -12,11 +15,12 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if(req.method == 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
         return res.status(200).json({});
     }
+    next();
 });
 
 app.use('/products', productsRoutes);
